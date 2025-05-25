2d6f1fdf7b6c1b8526b26c680eac0a8bd002afa2
SuomiGameHUB <info@suomigamehub.com>
2025-05-25 20:15:30 +0300

    Add files via upload

---
+++ Osa 1/Esimerkit/02_ParannaPelaajaa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParannaPelaajaa
+{
+    internal class Program
+    {
+        // Muuttuja pitämään kirjaa pelaajan elämäpisteistä
+        static int pelaajanElama = 50;
+
+        // Metodi pelaajan parantamiseen
+        static void ParannaPelaajaa()
+        {
+            int parannusMaara = 20; // Kuinka paljon elämää parannetaan
+            pelaajanElama = pelaajanElama + parannusMaara; // Lisätään parannusmäärä nykyiseen elämään
+            Console.WriteLine("Pelaajaa parannettiin! Uusi elämä: " + pelaajanElama);
+        }
+
+        // Päämetodi, josta ohjelman suoritus alkaa
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elämä alussa: " + pelaajanElama);
+
+            // Kutsutaan toimintoa "paranna" ensimmäisen kerran
+            ParannaPelaajaa();
+
+            // Kutsutaan toimintoa "paranna" toisen kerran
+            ParannaPelaajaa();
+
+            Console.ReadLine(); // Pysäyttää konsolin, jotta näet tulosteen
+        }
+    }
+}
