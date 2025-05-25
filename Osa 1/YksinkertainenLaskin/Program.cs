f7e697a291f12266685eb138d9a98808043aaada
SuomiGameHUB <info@suomigamehub.com>
2025-05-25 19:47:45 +0300

    Add files via upload

---
+++ Osa 1/YksinkertainenLaskin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YksinkertainenLaskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yksinkertainen laskin");
+            Console.WriteLine("Syötä ensimmäinen luku:");
+            string luku1Teksti = Console.ReadLine();
+            double luku1 = double.Parse(luku1Teksti);
+
+            Console.WriteLine("Syötä toinen luku:");
+            string luku2Teksti = Console.ReadLine();
+            double luku2 = double.Parse(luku2Teksti);
+
+            double summa = luku1 + luku2;
+
+            Console.WriteLine($"Summa on: {summa}");
+
+            Console.WriteLine("Paina Enter lopettaaksesi.");
+            Console.ReadLine();
+        }
+    }
+}
