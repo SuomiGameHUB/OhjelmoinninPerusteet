fe6f5293f4673a444ac0192e60bb3d4099b25572
SuomiGameHUB <info@suomigamehub.com>
2025-05-26 08:53:33 +0300

    Update 01_YksinkertainenLaskin.cs

--- Osa 1/Esimerkit/01_YksinkertainenLaskin.cs
+++ Osa 1/Esimerkit/01_YksinkertainenLaskin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace YksinkertainenLaskin
 {
@@ -9,13 +9,13 @@ namespace YksinkertainenLaskin
             Console.WriteLine("Yksinkertainen laskin");
             Console.WriteLine("Syötä ensimmäinen luku:");
             string luku1Teksti = Console.ReadLine();
-            double luku1 = double.Parse(luku1Teksti);
+            float luku1 = Single.Parse(luku1Teksti);
 
             Console.WriteLine("Syötä toinen luku:");
             string luku2Teksti = Console.ReadLine();
-            double luku2 = double.Parse(luku2Teksti);
+            float luku2 = Single.Parse(luku2Teksti);
 
-            double summa = luku1 + luku2;
+            float summa = luku1 + luku2;
 
             Console.WriteLine("Summa on: " + summa);
 
