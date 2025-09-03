dd8f18f0e4e8a9b90ef73eccb313c8a59b903244
SuomiGameHUB <info@suomigamehub.com>
2025-09-03 13:31:02 +0300

    Add files via upload

---
+++ Osa 2/Esimerkit/06_OlioOhjelmointi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OlioOhjelmointi
+{
+    public class Vihollinen
+    {
+        // Luokan ominaisuudet (properties) eli muuttujat, jotka kuvaavat vihollista
+        public int elamapisteet;
+        public string nimi;
+        public float nopeus;
+
+        // Konstruktori on metodi, jolla on sama nimi kuin luokalla
+        public Vihollinen(string alkunimi, int alkuelama, float alkunopeus)
+        {
+            nimi = alkunimi;
+            elamapisteet = alkuelama;
+            nopeus = alkunopeus;
+        }
+
+        // Luokan metodi eli toiminto, jonka vihollinen voi tehdä
+        public void Liiku()
+        {
+            // Koodi, joka liikuttaa vihollista
+            Console.WriteLine("Vihollinen " + nimi + " liikkuu nopeudella " + nopeus + ".");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Täällä ei tarvitse asettaa muuttujia erikseen
+            Vihollinen zombi = new Vihollinen("Zombi", 100, 1.5f);
+            Vihollinen luuranko = new Vihollinen("Luuranko", 80, 2.0f);
+
+            zombi.Liiku();
+            luuranko.Liiku();
+
+            Console.WriteLine("Paina Enter lopettaaksesi.");
+            Console.ReadLine();
+        }
+    }
+}
